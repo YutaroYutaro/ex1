0cb61eebbc0dc8bb84e8322e4aa69185df575ca7
yutaro <b3122047@gmail.com>
2018-12-17 12:41:19 +0900

    #6 userデータのバリデーションを追加．

--- app/php/Class/Validation.php
+++ app/php/Class/Validation.php
@@ -8,11 +8,17 @@
 
 class Validation
 {
-    public function MaxSize($str, $size) {
+    public function MaxSize($str, $size)
+    {
         return (mb_strlen($str, 'UTF-8') > $size);
     }
 
-    public function Required($str){
+    public function Required($str)
+    {
         return (empty($str));
+    }
+
+    public function EmailFormat($str) {
+        return !preg_match('/^([a-zA-Z0-9])+([a-zA-Z0-9-\._])*@([a-zA-Z0-9])+([a-zA-Z0-9\.])*([a-zA-Z0-9])+\.([a-zA-Z0-9])+$/', $str);
     }
 }
